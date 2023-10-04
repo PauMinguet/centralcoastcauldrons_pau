bb0a85a4e14f105daab799e55ed38de14c65c450
PauMinguet <pablitumm@gmail.com>
2023-10-03 22:13:59 -0700

    fix casting

--- src/api/carts.py
+++ src/api/carts.py
@@ -67,6 +67,5 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = " + str(num_red_potions)))
 
-
     return {"total_potions_bought": 1, 
             "total_gold_paid": 50}
